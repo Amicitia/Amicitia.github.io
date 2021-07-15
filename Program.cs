e3e35365abe7959fa158b14a14a1c23dc8a0b66d
ShrineFox <ShrineFox@users.noreply.github.com>
2021-07-15 07:00:30 -0400

    Further improve navbar & files page

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ namespace Amicitia.github.io
 
             // Create flowscript docs
             Page.FlowscriptDocs(indexPath);
-            // Create 404/files pages
+            // Create 404/forum/guides/files pages
             Page.Misc(indexPath);
 
             Console.WriteLine("Done!");
